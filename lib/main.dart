5fafcffa6abe7ceab70cb515b33ee921785d1107
Petar <baki_pfc@yahoo.com>
2021-11-03 02:07:17 +0100

    add new layout and styles to transaction cards

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,39 @@ class MyHomePage extends StatelessWidget {
           ),
           Column(
             children: transactions.map((tx) {
-              return Card(child: Text(tx.title));
+              return Card(
+                  child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2)),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      tx.amount.toString(),
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tx.title,
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        tx.date.toString(),
+                        style: const TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ));
             }).toList(),
           ),
         ],
