f45640ead9df724f2d6a5b31834cf5ba1c5e32a6
Petar <baki_pfc@yahoo.com>
2021-11-08 01:11:08 +0100

    fix formating

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -23,21 +23,22 @@ class Chart extends StatelessWidget {
         }
       }
 
-      print(DateFormat.E().format(weekDay));
-      print(totalSum);
-
-      return {'DAY': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    print(groupedTransactionValues);
     return Card(
       elevation: 6,
       margin: const EdgeInsets.all(20),
       child: Row(
-        children: const <Widget>[],
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
       ),
     );
   }
