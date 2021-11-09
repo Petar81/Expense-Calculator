c0fe8563185a72a043c2f36b5637fe6d8121f8fc
Petar <baki_pfc@yahoo.com>
2021-11-09 01:23:45 +0100

    add list tile widget to the transactions list layout

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -29,38 +29,26 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
-                    child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.purple, width: 2)),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        '\$${transactions[index].amount.toStringAsFixed(2)}',
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple),
+                  elevation: 5,
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('\$${transactions[index].amount}'),
+                        ),
                       ),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          transactions[index].title,
-                          style: const TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          DateFormat().format(transactions[index].date),
-                          style: const TextStyle(color: Colors.grey),
-                        )
-                      ],
-                    )
-                  ],
-                ));
+                    title: Text(
+                      transactions[index].title,
+                    ),
+                    subtitle: Text(
+                        DateFormat.yMMMEd().format(transactions[index].date)),
+                  ),
+                );
               },
               itemCount: transactions.length,
             ),
